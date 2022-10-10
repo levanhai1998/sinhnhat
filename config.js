4367b0702134fb28d156ad2eaec7230479a4a82e
levanhai1998 <86667736+levanhai1998@users.noreply.github.com>
2022-10-10 20:43:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Ánh';
+const giftUrl = '';
+const eventName = 'Chúc Mừng sinh nhật Ánh';
+const titleCard = 'Tặng Ánh';
+const contentCard = 'Chúc Ánh sinh nhật vui vẻ, gặp nhiều may mắn, tràn ngập niềm vui và những nụ cười';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '8.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
